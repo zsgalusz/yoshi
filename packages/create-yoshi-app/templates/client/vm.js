04a59983d09d425725224ec76c6a90877095e102
Artem Yavorsky <aqson@me.com>
2018-11-01 14:20:17 +0200

    Make bundle hash directrly in vm.js and pass it to index.vm template

--- packages/create-yoshi-app/templates/client/vm.js
+++ packages/create-yoshi-app/templates/client/vm.js
@@ -1,13 +1,19 @@
 const { Engine } = require('velocity');
+const { bundleTargets = [] } = require('yoshi-config');
+const { getId } = require('bundle-utils');
 
 const velocityData = require('./velocity.data.json');
 const velocityDataPrivate = require('./velocity.private.data.json');
 
 const engine = new Engine({ template: './src/index.vm' });
 
+const latestTarget = bundleTargets.length > 1 && bundleTargets[bundleTargets.length - 1];
+const modernBundleHash = latestTarget ? getId(latestTarget) : null;
+
 module.exports = (data) => {
   return engine.render(
     {
+      modernBundleHash,
       ...velocityData,
       ...velocityDataPrivate,
       ...data,
