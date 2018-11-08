301e95a52ff20e1000ccbac188cd933baae47c48
Gal Schlezinger <gal@spitfire.co.il>
2018-11-08 12:34:12 +0200

    Don't run tests in `start` by default. Fixes #524 (#738)

--- packages/yoshi/test/start.spec.js
+++ packages/yoshi/test/start.spec.js
@@ -91,7 +91,7 @@ describe('Aggregator: Start', () => {
             'package.json': fx.packageJson(),
             'pom.xml': fx.pom(),
           })
-          .spawn('start');
+          .spawn('start', '--with-tests');
 
         return checkStdout('Testing with Mocha');
       });
