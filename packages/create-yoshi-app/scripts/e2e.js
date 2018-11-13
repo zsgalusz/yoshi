d923f621017ede893e8c40eb3674631bf39d200a
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-11-13 23:01:23 +0200

    change start test to use puppeteer

--- packages/create-yoshi-app/scripts/e2e.js
+++ packages/create-yoshi-app/scripts/e2e.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const tempy = require('tempy');
 const execa = require('execa');
 const chalk = require('chalk');
@@ -90,15 +91,16 @@ const testTemplate = mockedAnswers => {
     });
 
     describe('npm start', () => {
+      let browser;
       let child;
       const serverPort = 3000;
-      const cdnPort = 3200;
-      afterEach(() => {
+      afterEach(async () => {
+        await browser.close();
         return killSpawnProcessAndHisChildren(child);
       });
-      it(`should run with local server`, async () => {
-        console.log('running npm start...');
 
+      it('should render a page without errors', async () => {
+        const consoleMessages = [];
         child = execa.shell('npm start', {
           cwd: tempDir,
           stdio,
@@ -106,76 +108,18 @@ const testTemplate = mockedAnswers => {
 
         await waitPort({ port: serverPort, output: 'silent' });
 
-        const { statusCode } = await new Promise(resolve =>
-          require('http').get(`http://localhost:${serverPort}`, resolve),
-        );
+        browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        page.on('console', msg => consoleMessages.push(msg));
+        await page.goto(`http://localhost:${serverPort}`);
+        const errors = consoleMessages.filter(e => e.type() !== 'debug');
 
-        expect(statusCode).toBe(200);
-      });
-
-      it(`should run with local cdn server`, async () => {
-        console.log('running npm start...');
-
-        child = execa.shell('npm start', {
-          cwd: tempDir,
-          stdio,
-        });
-
-        await waitPort({ port: cdnPort, output: 'silent' });
-
-        const { statusCode } = await new Promise(resolve =>
-          require('http').get(
-            `http://localhost:${cdnPort}/app.bundle.js`,
-            resolve,
-          ),
-        );
-
-        expect(statusCode).toBe(200);
+        expect(errors.map(e => e.text())).toEqual([]);
       });
     });
   });
 };
 
-<<<<<<< HEAD
-=======
-const publishMonorepo = async () => {
-  // Start in root directory even if run from another directory
-  process.chdir(path.join(__dirname, '../../..'));
-
-  const verdaccio = execa.shell('npx verdaccio --config verdaccio.yaml', {
-    stdio,
-  });
-
-  await waitPort({ port: 4873, output: 'silent' });
-
-  execa.shellSync(
-    `npx lerna exec 'npx npm-auth-to-token -u user -p password -e user@example.com -r "${testRegistry}"'`,
-    {
-      stdio,
-    },
-  );
-
-  execa.shellSync(
-    `npx lerna exec 'node ../../packages/create-yoshi-app/scripts/verifyPublishConfig.js'`,
-    {
-      stdio,
-    },
-  );
-
-  execa.shellSync(
-    `npx lerna publish --yes --force-publish=* --skip-git --cd-version=minor --exact --npm-tag=latest --registry="${testRegistry}"`,
-    {
-      stdio: 'inherit',
-    },
-  );
-
-  // Return a cleanup function
-  return () => {
-    execa.shellSync(`kill -9 ${verdaccio.pid}`);
-  };
-};
-
->>>>>>> a small refactor - change wait-port to use node api
 describe('create-yoshi-app + yoshi e2e tests', () => {
   let cleanup;
 
