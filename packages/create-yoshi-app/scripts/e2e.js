9f5f916d40937754c65d226441ceed6daf0a9798
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-11-18 17:20:39 +0200

    remove log

--- packages/create-yoshi-app/scripts/e2e.js
+++ packages/create-yoshi-app/scripts/e2e.js
@@ -129,7 +129,6 @@ const testTemplate = mockedAnswers => {
           error => error.type() !== 'debug',
         );
 
-        errors.map(e => console.log(e));
         expect(errors.map(error => error.text())).toEqual([]);
       });
     });
