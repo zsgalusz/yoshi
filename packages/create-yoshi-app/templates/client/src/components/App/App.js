b91b9ee38773093c0ee70f760f9cf0be63a08644
Gal Schlezinger <gal@spitfire.co.il>
2018-12-05 15:43:42 +0200

    Add FedOps to client

--- packages/create-yoshi-app/templates/client/src/components/App/App.js
+++ packages/create-yoshi-app/templates/client/src/components/App/App.js
@@ -1,3 +1,4 @@
+import * as fedops from 'fedops-logger';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
@@ -7,6 +8,15 @@ class App extends React.Component {
   static propTypes = {
     t: PropTypes.func,
   };
+
+  // The app load event is fired from the browser before the app is loaded.
+  // After the app is loaded, using the appLoaded function on the componentDidMount
+  // function to notify Grafana that the app was fully loaded.
+  componentDidMount() {
+    const logger = fedops.logger || fedops;
+    const fedopsLogger = logger.create('{%projectName%}');
+    fedopsLogger.appLoaded();
+  }
 
   render() {
     const { t } = this.props;
