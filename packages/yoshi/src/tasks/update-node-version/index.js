164bd7ffe37420a07e2d66cd3ebd855615d36b70
Yair Haimovitch <yairh@wix.com>
2019-01-06 16:02:23 +0200

    node version uses node semver instead of comparison

--- packages/yoshi/src/tasks/update-node-version/index.js
+++ packages/yoshi/src/tasks/update-node-version/index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+const semver = require('semver');
 
 module.exports = async ({ base = process.cwd() } = {}) => {
   const templateNvmrc = require.resolve('./templates/.nvmrc');
-  const templateVersion = fs.readFileSync(templateNvmrc);
+  const templateVersion = fs.readFileSync(templateNvmrc, 'utf-8');
 
   let projectVersion = '0';
 
@@ -13,7 +14,9 @@ module.exports = async ({ base = process.cwd() } = {}) => {
     projectVersion = fs.readFileSync(userNvmrcPath, 'utf8');
   } catch (e) {}
 
-  if (templateVersion > projectVersion) {
+  if (
+    semver.satisfies(semver.coerce(templateVersion), `>=v${projectVersion}`)
+  ) {
     console.log(`Upgrading node version @ ${templateVersion}`);
     fs.writeFileSync(userNvmrcPath, templateVersion);
   }
