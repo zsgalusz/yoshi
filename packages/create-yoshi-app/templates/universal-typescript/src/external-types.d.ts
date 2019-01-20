823bfad319f10b0692b8ad23c39bd98984616f01
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2019-01-20 18:45:11 +0200

    SVG as react component (#781)

--- packages/create-yoshi-app/templates/universal-typescript/src/external-types.d.ts
+++ packages/create-yoshi-app/templates/universal-typescript/src/external-types.d.ts
@@ -1,3 +1,14 @@
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+
+  const src: string;
+  export default src;
+}
 declare module '@wix/wix-run-mode';
 declare module 'serialize-javascript';
 declare module '*.scss';
