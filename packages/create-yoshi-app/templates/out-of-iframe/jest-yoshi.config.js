295b97a68cda6695edd4db0652b0e0cebdff59a6
Yury Michurin <yurym@wix.com>
2019-01-27 19:44:48 +0200

    Tell puppeteer ignore certificate errors

--- packages/create-yoshi-app/templates/out-of-iframe/jest-yoshi.config.js
+++ packages/create-yoshi-app/templates/out-of-iframe/jest-yoshi.config.js
@@ -3,4 +3,15 @@ module.exports = {
     command: 'node test/fake-server.js',
     port: 3100,
   },
+  puppeteer: {
+    // launch options: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#puppeteerlaunchoptions
+    // debugging tips: https://github.com/GoogleChrome/puppeteer#debugging-tips
+    devtools: false,
+    ignoreHTTPSErrors: true,
+    args: [
+      '--allow-insecure-localhost',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  },
 };
