49ae240e8c386f0ac8d3c3cd65996e3be59716a1
Ronen Amiel <ronen.amiel@gmail.com>
2019-06-25 18:11:46 +0300

    better experience for starting a single app

--- packages/yoshi/src/commands/start-monorepo.js
+++ packages/yoshi/src/commands/start-monorepo.js
@@ -19,15 +19,38 @@ if (cliArgs.production) {
   process.env.NODE_ENV = 'production';
 }
 
+const chalk = require('chalk');
 const loadPackages = require('yoshi-config/load-packages');
 const startSingleApp = require('./utils/start-single-app');
 
-const [, appName] = process.argv.slice(2);
+const [, appName] = cliArgs._;
 
 module.exports = async () => {
   const { apps } = await loadPackages();
 
-  const app = apps.find(lernaApp => lernaApp.name === appName);
+  let app;
+
+  // Find a specific app if a name was passed
+  if (appName) {
+    app = apps.find(lernaApp => lernaApp.name === appName);
+  }
+  // If there's only one app, start that
+  else if (apps.length === 1) {
+    app = apps[0];
+  }
+  // Otherwise, fail with an error
+  else {
+    console.log();
+    console.log(chalk.red(`Couldn't find an app to start`));
+    console.log(
+      chalk.red(
+        `Please choose which app to start by running \`npx yoshi start <appName>\``,
+      ),
+    );
+    console.log();
+    console.log(chalk.red('Aborting'));
+    process.exit(1);
+  }
 
   await startSingleApp(app, cliArgs);
 
