3c2a829d2c09f41ef06a92d4df185b9435456d73
Ronen Amiel <ronen.amiel@gmail.com>
2019-08-08 11:21:58 +0300

    types

--- packages/yoshi-server/src/client.ts
+++ packages/yoshi-server/src/client.ts
@@ -1,12 +1,22 @@
 import { JsonValue } from 'type-fest';
-import { Fn } from './wrap';
+import { Fn, Context } from './wrap';
 
 type Options = {
   baseUrl: string;
 };
 
-export default function create<
-  F extends { [key: string]: Fn<any, Array<JsonValue>, Promise<JsonValue>> }
->(obj: F, options: Options): F {
-  return obj;
+type JsonArgs = Array<JsonValue>;
+
+type JsonResult = Promise<JsonValue>;
+
+type Client = {
+  request: <Value extends JsonResult, Args extends JsonArgs>(
+    fn: Fn<Context, Args, Value>,
+    ...args: Args
+  ) => Value;
+};
+
+export default function create(options: Options): Client {
+  // @ts-ignore
+  return options;
 }
--- packages/yoshi-server/src/wrap.ts
+++ packages/yoshi-server/src/wrap.ts
@@ -3,7 +3,6 @@ import { JsonValue } from 'type-fest';
 import { WithAspects } from '@wix/wix-express-aspects';
 import Config from '@wix/wix-config';
 import { PetriClientFactory } from '@wix/wix-petri-client';
-// import config from 'yoshi-config';
 
 export type Context = {
   req: Request & WithAspects;
@@ -25,12 +24,7 @@ export type Fn<Context, Args extends JsonArgs, Result extends JsonResult> = (
 
 export function wrap<Value extends JsonResult, Args extends JsonArgs>(
   fn: Fn<Context, Args, Value>,
-): Fn<void, Args, Value> {
+): Fn<Context, Args, Value> {
   // @ts-ignore
   return fn;
 }
-
-// export async function conductExperiments(aspects: WithAspects) {
-//   const petri = context.petri.client(aspects);
-//   const experiments = await petri.conductAllInScope(config.unscopedName);
-// }
