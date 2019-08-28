7776a94d0d823c31823fa73373d003746cede491
Ronen Amiel <ronen.amiel@gmail.com>
2019-08-28 09:25:58 +0300

    wip

--- packages/yoshi-server/src/server.ts
+++ packages/yoshi-server/src/server.ts
@@ -1,4 +1,4 @@
-import { RequestListener } from 'http';
+import { RequestListener, IncomingMessage, ServerResponse } from 'http';
 import Youch from 'youch';
 import globby from 'globby';
 import SockJS from 'sockjs-client';
@@ -7,8 +7,19 @@ import importFresh from 'import-fresh';
 import serializeError from 'serialize-error';
 import config from 'yoshi-config';
 import { ROUTES_BUILD_DIR, BUILD_DIR } from 'yoshi-config/paths';
+import * as t from 'io-ts';
+import { PathReporter } from 'io-ts/lib/PathReporter';
+import { isLeft, mapLeft, map, either, Either } from 'fp-ts/lib/Either';
 import { getMatcher, relativeFilePath } from './utils';
 import Router, { route, Route } from './router';
+
+const ClientRequest = t.type({
+  fileName: t.string,
+  methodName: t.string,
+  args: t.array(t.any),
+});
+
+const BatchClientRequest = t.array(ClientRequest);
 
 export default class Server {
   private context: any;
@@ -40,6 +51,16 @@ export default class Server {
     }
   }
 
+  private async validateRequestBody<O>(
+    req: IncomingMessage,
+    codec: t.Type<O, any, any>,
+  ): Promise<Either<Array<string>, O>> {
+    const body = await json(req);
+    const result = codec.decode(body);
+
+    return either.mapLeft(result, () => PathReporter.report(result));
+  }
+
   private generateRoutes(): Array<Route> {
     const functions = this.createFunctions();
     const dynamicRoutes = this.createDynamicRoutes();
@@ -48,47 +69,39 @@ export default class Server {
       {
         match: route('/_api_'),
         fn: async (req, res) => {
-          const { methodName, fileName, args } = await json(req);
+          const data = await this.validateRequestBody(req, ClientRequest);
 
-          const matched = functions[fileName][methodName];
-
-          if (matched) {
-            const fnThis = {
-              context: this.context,
-              config: this.config,
-              req,
-              res,
-            };
-
-            const result = await matched.__fn__.call(fnThis, args);
-
-            return send(res, 200, result);
+          if (isLeft(data)) {
+            return send(res, 406, data.left);
           }
 
-          throw new Error('Could not find method');
+          const result = await this.runServerFunction({
+            req,
+            res,
+            functions,
+            fnData: data.right,
+          });
+
+          return send(res, 200, result);
         },
       },
       {
         match: route('/_batch_'),
         fn: async (req, res) => {
-          const data = await json(req);
+          const data = await this.validateRequestBody(req, BatchClientRequest);
+
+          if (isLeft(data)) {
+            return send(res, 406, data.left);
+          }
 
           const results = await Promise.all(
-            data.map(async ({ fileName, methodName, args }: any) => {
-              const matched = functions[fileName][methodName];
-
-              if (matched) {
-                const fnThis = {
-                  context: this.context,
-                  config: this.config,
-                  req,
-                  res,
-                };
-
-                return matched.__fn__.call(fnThis, args);
-              }
-
-              throw new Error('Could not find method');
+            data.right.map(async fnData => {
+              return this.runServerFunction({
+                req,
+                res,
+                functions,
+                fnData,
+              });
             }),
           );
 
@@ -97,6 +110,33 @@ export default class Server {
       },
       ...dynamicRoutes,
     ];
+  }
+
+  private async runServerFunction({
+    req,
+    res,
+    functions,
+    fnData,
+  }: {
+    req: IncomingMessage;
+    res: ServerResponse;
+    functions: { [filename: string]: any };
+    fnData: t.TypeOf<typeof ClientRequest>;
+  }) {
+    const matched = functions[fnData.fileName][fnData.methodName];
+
+    if (!matched) {
+      throw new Error('Could not find function');
+    }
+
+    const fnThis = {
+      context: this.context,
+      config: this.config,
+      req,
+      res,
+    };
+
+    return matched.__fn__.call(fnThis, fnData.args);
   }
 
   public handle: RequestListener = async (req, res) => {
